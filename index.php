9f35c845fc1a8f9580de41fc2043c42af81880eb
IvanWu2015 <wx.1@163.com>
2017-02-21 16:18:53 +0800

    edit in vim

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 /**
  *      wuxin
- * this is the first git file
+ * This is the first git file
+ * Now is the branch master_1.0
+ * Vim is the best editor
  */
 
  say("hello world!"); 
  
  function say($string) {
 	 print_r($string);
-	 return true;
+	 return false;
  }
